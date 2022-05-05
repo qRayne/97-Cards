ec9691974056653df645967044b4b1140adfcb1b
Rayan <kennafrayanerachid@gmail.com>
2022-05-05 19:22:34 -0400

    added 3 new linearLayout for en-tête

--- app/src/main/java/com/example/a97cards/MainActivity.java
+++ app/src/main/java/com/example/a97cards/MainActivity.java
@@ -2,13 +2,35 @@ package com.example.a97cards;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Les variables
+    Button commencerPartie;
+    Ecouteur ec;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        commencerPartie = findViewById(R.id.buttonPlay);
+        ec = new Ecouteur();
+
+        commencerPartie.setOnClickListener(ec);
+    }
+
+    private class Ecouteur implements View.OnClickListener{
+
+        @Override
+        public void onClick(View v) {
+            // si le joueur clicke sur une nouvelle partie, alors il sera rediriger vers la partie
+            if (v.equals(commencerPartie))
+                startActivity(new Intent(MainActivity.this, JeuActivity.class));
+        }
     }
 }

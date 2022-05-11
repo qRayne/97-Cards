95716d06a5d8578dadbd155fd37315f0289ed42d
Rayan <kennafrayanerachid@gmail.com>
2022-05-11 18:42:12 -0400

    finished front-end and added GameOveractivity

---
+++ app/src/main/java/com/example/a97cards/GameOverActivity.java
@@ -0,0 +1,36 @@
+package com.example.a97cards;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class GameOverActivity extends AppCompatActivity {
+
+    Button rejouer;
+    TextView scoreFinal;
+    Ecouteur ec;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_over);
+
+        rejouer = findViewById(R.id.buttonRejouer);
+        scoreFinal = findViewById(R.id.textViewScoreFinal);
+        ec = new Ecouteur();
+    }
+
+    private class Ecouteur implements View.OnClickListener{
+
+        @Override
+        public void onClick(View v) {
+            if (v.equals(rejouer))
+                startActivity(new Intent(GameOverActivity.this,JeuActivity.class));
+        }
+    }
+}
